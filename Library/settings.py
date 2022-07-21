be010967c4ed568104fd426bcd91c4ef28d4c392
kiiza-quark <92211594+kiiza-quark@users.noreply.github.com>
2022-07-21 09:41:20 +0300

    Update settings.py

--- Library/settings.py
+++ Library/settings.py
@@ -98,7 +98,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-#TIME_ZONE = 'America'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
@@ -121,7 +121,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
